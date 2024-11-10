f4c0513a72c26d87343c8a3ed9eccee0893dc42b
Tomás Caruso <143461944+tomasccc@users.noreply.github.com>
2024-11-10 00:37:08 -0300

    Update inventory.routes.ts

--- src/app/inventories/inventory.routes.ts
+++ src/app/inventories/inventory.routes.ts
@@ -2,20 +2,19 @@ import { Routes } from "@angular/router";
 import { InventoryEmployeeProviderHomeComponent } from "./inventory-employee-provider-home/inventory-employee-provider-home.component";
 import { IepDetailTableComponent } from "./iep-inventory/components/iep-detail-table/iep-detail-table.component";
 import { IepInventoryComponent } from "./iep-inventory/components/iep-inventory/iep-inventory.component";
-import { IepProductComponent } from "./iep-inventory/components/iep-product/iep-product.component";
 import { IepStockIncreaseComponent } from "./iep-inventory/components/iep-stock-increase/iep-stock-increase.component";
 import { IepTableComponent } from "./iep-inventory/components/iep-table/iep-table.component";
 import { IepWarehouseMovementSearchComponent } from "./iep-inventory/components/iep-warehouse-movement-search/iep-warehouse-movement-search.component";
+import { IepCategoriesListComponent } from "./iep-inventory/components/iep-categories-list/iep-categories-list.component";
+import { IepChartsInventoryComponent } from "./iep-inventory/components/iep-charts-inventory/iep-charts-inventory.component";
 
 export const INVENTORY_ROUTES: Routes = [
  { path: '', component: InventoryEmployeeProviderHomeComponent }, //agregado para desarrollar
     
  { path: 'inventory', component: IepInventoryComponent },
-    { path: 'inventory-detail', component: IepDetailTableComponent },
-    { path: 'register-product', component: IepProductComponent },
     { path: 'stock-increase', component: IepStockIncreaseComponent },
     { path: 'stock-movements-history', component: IepTableComponent },
     { path: 'warehouse-movements', component: IepWarehouseMovementSearchComponent },
-    { path: 'categories', component: InventoryEmployeeProviderHomeComponent }, //Remplazar por el componente del listad de categorias
-    { path: 'dashboard', component: InventoryEmployeeProviderHomeComponent }, // Cambiar por el componente correspondiente
+    { path: 'categories', component: IepCategoriesListComponent }, 
+    { path: 'dashboard', component: IepChartsInventoryComponent }, 
 ];
