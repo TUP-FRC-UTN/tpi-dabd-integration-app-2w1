61cbb15f3afe5ab1575734a9e0ec33ce30c379a2
Tomás Caruso <143461944+tomasccc@users.noreply.github.com>
2024-11-10 12:04:02 -0300

    .

--- src/app/inventories/inventory-employee-provider-home/inventory-employee-provider-home.component.ts
+++ src/app/inventories/inventory-employee-provider-home/inventory-employee-provider-home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MOCKUsersNavbarComponent } from '../common-components/users-navbar/users-navbar.component';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inventory-employee-provider-home',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, MOCKUsersNavbarComponent],
+  imports: [RouterModule, RouterOutlet],
   
   templateUrl: './inventory-employee-provider-home.component.html',
   styleUrl: './inventory-employee-provider-home.component.scss'
