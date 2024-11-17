536bb84d573cbfda665da657c53bcd4cc789a8b5
404888_MONASTERIO <404888@tecnicatura.frc.utn.edu.ar>
2024-11-17 05:35:06 -0300

    Removed unnecesary import

--- src/app/entries/entry.routes.ts
+++ src/app/entries/entry.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from "@angular/router";
 import { AccessGlobalReportComponent } from "./components/entries_reports/entries-global-report/access-global-report.component";
 import { AccessVehiclesViewComponent } from "./components/access_vehicles-register/access-vehicles-view/access-vehicles-view.component";
-import { AccessVisitorRegistryComponent } from "./components/access_visitors/access-visitor-registry/access-visitor-registry.component";
 import { AccessContainerVisitorsRegistrationComponent } from "./components/access_visitors/access_visitors_register/access-container-visitors-registration/access-container-visitors-registration.component";
 import { MetricsComponent } from "./components/metrics/metrics.component";
 
 export const ENTRY_ROUTES: Routes = [
-    { path: 'reports', component: AccessGlobalReportComponent },
+    { path: 'reports', component: AccessGlobalReportComponent},
     { path: 'visitor', component: AccessContainerVisitorsRegistrationComponent }, 
     { path: 'vehicles', component: AccessVehiclesViewComponent },
     { path: 'dashboard', component: MetricsComponent },
