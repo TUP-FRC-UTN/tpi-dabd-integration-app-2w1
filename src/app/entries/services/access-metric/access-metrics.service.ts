432dc13c0641b7319524dde2e6360dccad31cc7f
404888_MONASTERIO <404888@tecnicatura.frc.utn.edu.ar>
2024-11-17 02:31:38 -0300

    Migrated from repository

--- src/app/entries/services/access-metric/access-metrics.service.ts
+++ src/app/entries/services/access-metric/access-metrics.service.ts
@@ -1,21 +1,167 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AccessMonthlyVisitorCount } from '../../models/access-visitors-count/access-monthly-visitor-count';
+import { AccessCount, AccessMetricsDTO, DayOfWeekMetricEntryDTO, DayOfWeekMetricExitDTO, HourlyMetricDTO, TopUser, UserTypeMetricDTO, UtilizationRateResponse } from '../../models/access-metric/metris';
 @Injectable({
   providedIn: 'root'
 })
 export class AccessMetricsService {
-  private apiUrl = 'http://localhost:8090';
+
+  private baseUrl = 'http://localhost:8090';
   
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getMonthlyVisitorCounts(): Observable<AccessMonthlyVisitorCount[]> {
-    return this.http.get<AccessMonthlyVisitorCount[]>(this.apiUrl + "/monthly_counts");
+  getDailyAccessData(): Observable<{ date: string, count: number }[]> {
+    return this.http.get<{ date: string, count: number }[]>(`${this.baseUrl}/daily-access-count`);
   }
 
-  getMonthlyVisitorCountsByType(): Observable<AccessMonthlyVisitorCount[]> {
-    return this.http.get<AccessMonthlyVisitorCount[]>(this.apiUrl + "/monthly-visitors-by-type");
+  getDailyExitData(): Observable<{ date: string, count: number }[]> {
+    return this.http.get<{ date: string, count: number }[]>(`${this.baseUrl}/daily-exit-count`);
   }
 
+  getAccessCountByUserTypeFilter(
+    year: number,
+    startMonth: number,
+    endMonth: number
+  ): Observable<AccessCount[]> {
+    return this.http.get<AccessCount[]>(
+      `${this.baseUrl}/access-count-by-user-type-filter`,
+      {
+        params: {
+          year: year.toString(),
+          startMonth: startMonth.toString(),
+          endMonth: endMonth.toString()
+        }
+      }
+    );
+  }
+  
+  getExitCountByUserTypeFilter(
+    year: number,
+    startMonth: number,
+    endMonth: number
+  ): Observable<AccessCount[]> {
+    return this.http.get<AccessCount[]>(
+      `${this.baseUrl}/exit-count-by-user-type-filter`,
+      {
+        params: {
+          year: year.toString(),
+          startMonth: startMonth.toString(),
+          endMonth: endMonth.toString()
+        }
+      }
+    );
+  }
+  
+getTotalCountsMovementsByFilter(   
+  year: number,
+  startMonth: number,
+  endMonth: number
+): Observable<any[]> {
+  return this.http.get<any[]>(
+    `${this.baseUrl}/total-access-and-exit-counts`,
+    {
+      params: {
+        year: year.toString(),
+        startMonth: startMonth.toString(),
+        endMonth: endMonth.toString()
+      }
+    }
+  );
+}
+
+  getMovementCountsFilter(
+    year: number, 
+    startMonth: number, 
+    endMonth: number, 
+    filterType: 'ingresos' | 'egresos' | 'total'
+  ): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/access-exit-count-by-day-filter`,
+      {
+        params: {
+          year: year.toString(),
+          startMonth: startMonth.toString(),
+          endMonth: endMonth.toString(),
+          filterType: filterType 
+        }
+      }
+    );
+  }
+
+  getAccessCountByWeekAndDayOfWeek(): Observable<DayOfWeekMetricEntryDTO> {
+    return this.http.get<DayOfWeekMetricEntryDTO>(`${this.baseUrl}/day-with-most-accesses`);
+  }
+
+
+  getExitCountByWeekAndDayOfWeek(): Observable<DayOfWeekMetricExitDTO> {
+    return this.http.get<DayOfWeekMetricExitDTO>(`${this.baseUrl}/day-with-most-exits`);
+  }
+
+  getThisMonthlyAccessCount(): Observable<number> {
+    return this.http.get<number>(`${this.baseUrl}/this-monthly-access-count`);
+  }
+
+  getThisMonthlyExitCount(): Observable<number> {
+    return this.http.get<number>(`${this.baseUrl}/this-monthly-exit-count`);
+  }
+
+
+  getTotalEntriesForCurrentYear(): Observable<number> {
+    return this.http.get<number>(this.baseUrl +'/total-entries-this-year');
+  }
+
+  getMonthWithMostEntries(): Observable<any> {
+    return this.http.get<any>(this.baseUrl + "/month-with-most-entries");
+  }
+
+  getTotalExitsForCurrentYear(): Observable<number> {
+    return this.http.get<number>(this.baseUrl +'/total-exits-current-year');
+  }
+
+  getMonthWithMostExitss(): Observable<any> {
+    return this.http.get<any>(this.baseUrl + "/month-with-most-exits");
+  }
+
+  getTopUsers(startMonth: number, endMonth: number, year: number): Observable<TopUser[]> {
+    const params = {
+      startMonth: startMonth.toString(),
+      endMonth: endMonth.toString(),
+      year: year.toString()
+    };
+    return this.http.get<TopUser[]>(this.baseUrl + '/top-users-entries-exits', { params });
+  }
+
+  getGuardWithMostExits(startYear: number, startMonth: number, endYear: number, endMonth: number): Observable<any> {
+    return this.http.get<any>(this.baseUrl + '/guard-with-most-exits', {
+      params: {
+        startYear: startYear.toString(),
+        startMonth: startMonth.toString(),
+        endYear: endYear.toString(),
+        endMonth: endMonth.toString()
+      }
+    });
+  }
+
+  getGuardWithMostEntries(startYear: number, startMonth: number, endYear: number, endMonth: number): Observable<any> {
+    return this.http.get<any>(this.baseUrl + '/guard-with-most-entries', {
+      params: {
+        startYear: startYear.toString(),
+        startMonth: startMonth.toString(),
+        endYear: endYear.toString(),
+        endMonth: endMonth.toString()
+      }
+    });
+  }
+
+  getNeighborWithMostAuthorizations(startYear: number, startMonth: number, endYear: number, endMonth: number): Observable<any> {
+    return this.http.get<any>(this.baseUrl + '/neighbor-with-most-invitations', {
+      params: {
+        startYear: startYear.toString(),
+        startMonth: startMonth.toString(),
+        endYear: endYear.toString(),
+        endMonth: endMonth.toString()
+      }
+    });
+  }
 }
