a4f49ad100417e10f1f67b3c7e63d8d12c5e2d83
CamilaLopez <113983@tecnicatura.frc.utn.edu.ar>
2024-11-16 21:28:08 -0300

    Refactor plots

--- src/app/users/users-components/plots/users-list-plots/users-list-plots.component.ts
+++ src/app/users/users-components/plots/users-list-plots/users-list-plots.component.ts
@@ -1,29 +1,23 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
-import { UserGet } from '../../../users-models/users/UserGet';
-import { CommonModule, getLocaleMonthNames } from '@angular/common';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserService } from '../../../users-servicies/user.service';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 import * as XLSX from 'xlsx';
 import $ from 'jquery';
 import 'datatables.net';
 import 'datatables.net-bs5';
-import { Router, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
-import { PlotModel } from '../../../users-models/plot/Plot';
 import { PlotService } from '../../../users-servicies/plot.service';
 import { GetPlotModel } from '../../../users-models/plot/GetPlot';
 import { UsersModaInfoPlotComponent } from '../users-moda-info-plot/users-moda-info-plot.component';
 import { PlotStateModel } from '../../../users-models/plot/PlotState';
 import { PlotTypeModel } from '../../../users-models/plot/PlotType';
 import { OwnerService } from '../../../users-servicies/owner.service';
-import { map } from 'rxjs/operators';
 import { Owner } from '../../../users-models/owner/Owner';
-import { LoginComponent } from '../../../../common/components/users-login/login.component';
-import { CustomSelectComponent } from '../../../../common/components/custom-select/custom-select.component'; 
+import { CustomSelectComponent } from '../../../../common/components/custom-select/custom-select.component';
+import { SuscriptionManagerService } from '../../../../common/services/suscription-manager.service';
 
 @Component({
   selector: 'app-users-list-plots',
@@ -32,7 +26,7 @@ import { CustomSelectComponent } from '../../../../common/components/custom-sele
   templateUrl: './users-list-plots.component.html',
   styleUrl: './users-list-plots.component.css'
 })
-export class UsersListPlotsComponent {
+export class UsersListPlotsComponent implements OnInit, OnDestroy {
   plots: GetPlotModel[] = [];
   private readonly plotService = inject(PlotService);
   private readonly ownerService = inject(OwnerService);
@@ -43,48 +37,24 @@ export class UsersListPlotsComponent {
   selectType = new FormControl();
   selectState = new FormControl();
 
-
   plotTypes: string[] = [];
   plotStatus: string[] = [];
   typesForFilter: any[] = [];
   statusForFilter: any[] = [];
 
+  private readonly suscriptionService = inject(SuscriptionManagerService);
 
   constructor(private router: Router, private modal: NgbModal) { }
 
+  ngOnDestroy(): void {
+    this.suscriptionService.unsubscribeAll();
+  }
+
   async ngOnInit() {
-    this.plotService.getAllStates().subscribe({
-      next: (data: PlotStateModel[]) => {
+    this.loadAllPlotsStates();
+    this.loadAllPlotsTypes();
 
-        this.plotStatus = data.map(state => state.name);
-
-        this.statusForFilter = data.map(r => ({
-          value: r.name,
-          name: r.name
-        }));
-
-
-      },
-      error: (error) => {
-        console.error('Error al cargar los estados:', error);
-      }
-    });
-
-    this.plotService.getAllTypes().subscribe({
-      next: (data: PlotTypeModel[]) => {
-        this.plotTypes = data.map(type => type.name);
-
-        this.typesForFilter = data.map(r => ({
-          value: r.name,
-          name: r.name
-        }));
-      },
-      error: (error) => {
-        console.error('Error al cargar los tipos:', error);
-      }
-    });
-
-    this.plotService.getAllPlots().subscribe({
+    const sus = this.plotService.getAllPlots().subscribe({
       next: async (data: GetPlotModel[]) => {
         this.plots = data;
 
@@ -177,7 +147,7 @@ export class UsersListPlotsComponent {
           // Asignar eventos a los botones "Ver más" y "Editar"
           $('#myTable').on('click', '.view-plot', (event) => {
             const plotId = $(event.currentTarget).data('id');
-            this.abrirModal(plotId);
+            this.openModal(plotId);
           });
 
           $('#myTable').on('click', '.edit-plot', (event) => {
@@ -192,13 +162,59 @@ export class UsersListPlotsComponent {
       }
     });
 
+    //Agregar servicio
+    this.suscriptionService.addSuscription(sus);
+
     this.selectType.setValue('');
     this.selectState.setValue('');
   }
+  //--------------------------------------------------Carga de datos--------------------------------------------------
 
+  //Cargar estados de los lotes
+  loadAllPlotsStates() {
+    const sus = this.plotService.getAllStates().subscribe({
+      next: (data: PlotStateModel[]) => {
+
+        this.plotStatus = data.map(state => state.name);
+
+        this.statusForFilter = data.map(r => ({
+          value: r.name,
+          name: r.name
+        }));
+      },
+      error: (error) => {
+        console.error('Error al cargar los estados:', error);
+      }
+    });
+
+    //Agregar servicio
+    this.suscriptionService.addSuscription(sus);
+  }
+
+  //Cargar tipos de los lotes
+  loadAllPlotsTypes() {
+    const sus = this.plotService.getAllTypes().subscribe({
+      next: (data: PlotTypeModel[]) => {
+        this.plotTypes = data.map(type => type.name);
+
+        this.typesForFilter = data.map(r => ({
+          value: r.name,
+          name: r.name
+        }));
+      },
+      error: (error) => {
+        console.error('Error al cargar los tipos:', error);
+      }
+    });
+
+    //Agregar servicio
+    this.suscriptionService.addSuscription(sus);
+  }
+
+  //Obtener el propietario por id
   async showOwner(plotId: number): Promise<string> {
     return new Promise<string>((resolve) => {
-      this.ownerService.getOwnerByPlotId(plotId).subscribe({
+      const sus = this.ownerService.getOwnerByPlotId(plotId).subscribe({
         next: (data: Owner[]) => {
           if (data.length > 0) {
             resolve(data[0].lastname + ', ' + data[0].name);
@@ -211,18 +227,22 @@ export class UsersListPlotsComponent {
           resolve('Error al cargar propietario');
         }
       });
+      //Agregar servicio
+      this.suscriptionService.addSuscription(sus);
     });
+
   }
 
+  //--------------------------------------------------Filtros------------------------------------------------
 
   //Filtrat por tipo
   updateFilterType(options: any[]) {
-   // Asignamos directamente los roles emitidos
+    // Asignamos directamente los roles emitidos
     var optionsFilter = options.map((option: any) => option).join(' ');
     console.log(optionsFilter);
-    
+
     const table = $('#myTable').DataTable();
-    
+
     table.column(4).search(optionsFilter ?? '').draw();
   }
 
@@ -237,80 +257,67 @@ export class UsersListPlotsComponent {
     const table = $('#myTable').DataTable();
     table.column(5).search(this.selectState.value ?? '').draw();
   }
- 
 
   //Filtrar por estado
   updateFilterState(options: any[]) {
     var optionsFilter = options.map((option: any) => option).join(' ');
     console.log(optionsFilter);
-    
+
     const table = $('#myTable').DataTable();
-    
+
     table.column(5).search(optionsFilter ?? '').draw();
   }
 
-  getContentBetweenArrows(input: string): string[] {
-    const matches = [...input.matchAll(/>(.*?)</g)];
-    const cleanedResults = matches
-      .map(match => match[1].trim()) // Elimina los espacios al inicio y al final de cada elemento
-      .filter(content => content !== ""); // Filtra los elementos que solo sean espacios o estén vacíos
-    
-    console.log(cleanedResults); // Verifica el resultado con los logs
-  
-    return cleanedResults;
-  }
-  
-
+  //Limpiar filtros
   resetFilters() {
-    // Reiniciar el valor del control de rol
+    //Reiniciar el valor del control de rol
     this.selectType.setValue('');
     this.selectState.setValue('');
 
-    // Limpiar el campo de búsqueda general y el filtro de la columna de tipo
+    //Limpiar el campo de búsqueda general y el filtro de la columna de tipo
     const searchInput = document.querySelector('#myTable_filter input') as HTMLInputElement;
     if (searchInput) {
-      searchInput.value = ''; // Limpiar el valor del input de búsqueda general
+      searchInput.value = ''; 
     }
-
-    // Obtener la instancia de DataTable
     const table = $('#myTable').DataTable();
 
     // Limpiar búsqueda y filtros
-    table.search('').draw(); // Limpiar búsqueda general
-    table.column(4).search('').draw(); // Limpiar filtro de tipo
-    table.column(5).search('').draw(); // Limpiar filtro de tipo
+    table.search('').draw();
+    table.column(4).search('').draw(); 
+    table.column(5).search('').draw(); 
   }
 
+  //--------------------------------------------------Exportar------------------------------------------------
 
-  //Exporta a pdf la tabla, si esta filtrada solo exporta los datos filtrados
+  //Exporta a pdf la tabla
   exportPdf() {
-    // Mantener la orientación del documento en portrait
+    //Mantener la orientación del documento en portrait
     const doc = new jsPDF('portrait');
 
-    // Agregar título centrado con la fecha actual a la derecha
+    //Agregar título centrado con la fecha actual a la derecha
     const title = 'Lista de Lotes';
     doc.setFontSize(18);
     doc.text(title, 15, 20);
     doc.setFontSize(12);
 
     const today = new Date();
-    const formattedDate = 
-    today.getDate().toString().padStart(2, '0') + '/' + 
-    (today.getMonth() + 1).toString().padStart(2, '0') + '/' + 
-    today.getFullYear();
+    const formattedDate =
+      today.getDate().toString().padStart(2, '0') + '/' +
+      (today.getMonth() + 1).toString().padStart(2, '0') + '/' +
+      today.getFullYear();
     doc.text(`Fecha: ${formattedDate}`, 15, 30);
 
-    // Configuración del título
+    //Configuración del título
     const titleText = `${title}`;
 
-    // Definir las columnas de la tabla
+    //Definir las columnas de la tabla
     const columns = ['Lote', 'Manzana', 'M2 Totales', 'M2 Construidos', 'Tipo', 'Estado', 'Propietario'];
 
-    // Obtener los datos filtrados en la tabla HTML
+    //Obtener los datos filtrados en la tabla HTML
     const table = $('#myTable').DataTable();
     const visibleRows = table.rows({ search: 'applied' }).data().toArray();
 
-    // Mapear los datos visibles para el PDF
+    //Mapear los datos visibles para el PDF
     const rows = visibleRows.map((row: any) => [
       `${this.getContentBetweenArrows(row[0])}`,
       `${this.getContentBetweenArrows(row[1])}`,
@@ -344,18 +351,19 @@ export class UsersListPlotsComponent {
     doc.save(fileName);
   }
 
+  //Exporta a excel la tabla
   exportExcel() {
-    // Obtenemos los datos visibles de la tabla (basados en los filtros aplicados)
+    //Obtenemos los datos visibles de la tabla (basados en los filtros aplicados)
     const table = $('#myTable').DataTable();
     const visibleRows = table.rows({ search: 'applied' }).data().toArray();
 
     const today = new Date();
-    const formattedDate = 
-    today.getDate().toString().padStart(2, '0') + '/' + 
-    (today.getMonth() + 1).toString().padStart(2, '0') + '/' + 
-    today.getFullYear();
-  
-    // Preparamos los datos en formato adecuado para Excel
+    const formattedDate =
+      today.getDate().toString().padStart(2, '0') + '/' +
+      (today.getMonth() + 1).toString().padStart(2, '0') + '/' +
+      today.getFullYear();
+
+    //Preparamos los datos en formato adecuado para Excel
     const rows = visibleRows.map((row: any) => [
       `${this.getContentBetweenArrows(row[0])}`,
       `${this.getContentBetweenArrows(row[1])}`,
@@ -363,27 +371,28 @@ export class UsersListPlotsComponent {
       `${this.getContentBetweenArrows(row[3])}`,
       `${this.getContentBetweenArrows(row[4])}`,
       `${this.getContentBetweenArrows(row[5])}`,
-      `${row[6]}`  // Propietario
+      `${row[6]}`  //Propietario
     ]);
-  
-    // Definimos las columnas para el archivo Excel
+
+    //Definimos las columnas para el archivo Excel
     const header = ['Lote', 'Manzana', 'M2 Totales', 'M2 Construidos', 'Tipo', 'Estado', 'Propietario'];
-  
-    // Creamos la hoja de Excel
+
+    //Creamos la hoja de Excel
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([header, ...rows]);
-  
-    // Creamos un libro de trabajo con la hoja
+
+    //Creamos un libro de trabajo con la hoja
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Lista de Lotes');
-  
-    // Exportamos el archivo Excel
+
+    //Exportamos el archivo Excel
     const fileName = `${formattedDate}_listado_lotes.xlsx`;
     XLSX.writeFile(wb, fileName);
   }
 
+  //--------------------------------------------------Modal------------------------------------------------
 
-
-  async abrirModal(plotId: number) {
+  //Abre el modal con la información del lote
+  async openModal(plotId: number) {
     console.log("Esperando a que userModal se cargue...");
 
     // Espera a que se cargue el usuario seleccionado
@@ -408,28 +417,19 @@ export class UsersListPlotsComponent {
     }
   }
 
-  addPlot() {
-    this.router.navigate(['/home/plots/add'])
-  }
-
-  redirectEdit(id: number) {
-    this.router.navigate(['/home/plots/edit', id])
-  }
-
   // Busca el user y se lo pasa al modal
   plotModel: GetPlotModel = new GetPlotModel();
   selectUser(id: number): Promise<GetPlotModel> {
-    // Mostrar SweetAlert de tipo 'cargando'
     return new Promise((resolve, reject) => {
       this.plotService.getPlotById(id).subscribe({
         next: (data: GetPlotModel) => {
           this.plotModel = data;
-          Swal.close(); // Cerrar SweetAlert
-          resolve(data); // Resuelve la promesa cuando los datos se cargan
+          Swal.close();
+          resolve(data);
         },
         error: (error) => {
           console.error('Error al cargar el lote:', error);
-          reject(error); // Rechaza la promesa si ocurre un error
+          reject(error);
           Swal.close();
           Swal.fire({
             icon: 'error',
@@ -442,8 +442,10 @@ export class UsersListPlotsComponent {
     });
   }
 
-  showPlotType(plotType: any): string {
+  //--------------------------------------------------Estilos------------------------------------------------
 
+  //Establece un estilo según el tipo de lote
+  showPlotType(plotType: any): string {
     let color: string = ''
 
     switch (plotType) {
@@ -460,8 +462,8 @@ export class UsersListPlotsComponent {
     return `<div class="d-flex justify-content-center"> <span class='badge rounded-pill ${color}'>${plotType}</span> </div>`;
   }
 
+  //Establece un estilo según el estado del lote
   showPlotState(plotState: any): string {
-
     let color: string = ''
 
     switch (plotState) {
@@ -478,4 +480,26 @@ export class UsersListPlotsComponent {
 
     return `<div class="d-flex justify-content-center"> <span class='${color} badge rounded-pill'>${plotState}</span></div>`;
   }
+
+  //Quedarse sólo con el contenido entre las etiquetas de los elementos
+  getContentBetweenArrows(input: string): string[] {
+    const matches = [...input.matchAll(/>(.*?)</g)];
+    const cleanedResults = matches
+      .map(match => match[1].trim()) //Elimina los espacios al inicio y al final de cada elemento
+      .filter(content => content !== ""); //Filtra los elementos que solo sean espacios o estén vacíos
+
+    return cleanedResults;
+  }
+
+  //--------------------------------------------------Redirecciones------------------------------------------------
+
+    // Redirige a la vista de agregar lote
+    addPlot() {
+      this.router.navigate(['/home/plots/add'])
+    }
+  
+    // Redirige a la vista para editar l
+    redirectEdit(id: number) {
+      this.router.navigate(['/home/plots/edit', id])
+    }
 }
--- src/app/users/users-components/plots/users-update-plot/users-update-plot.component.ts
+++ src/app/users/users-components/plots/users-update-plot/users-update-plot.component.ts
@@ -1,17 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { PlotService } from '../../../users-servicies/plot.service';
 import { PlotTypeModel } from '../../../users-models/plot/PlotType';
 import { PlotStateModel } from '../../../users-models/plot/PlotState';
-import { PlotModel } from '../../../users-models/plot/Plot';
 import { ActivatedRoute, Router } from '@angular/router';
-import { GetPlotModel } from '../../../users-models/plot/GetPlot';
 import { PutPlot } from '../../../users-models/plot/PutPlot';
 import { FileDto } from '../../../users-models/owner/FileDto';
 import { FileService } from '../../../users-servicies/file.service';
-import { Title } from '@angular/platform-browser';
+import { SuscriptionManagerService } from '../../../../common/services/suscription-manager.service';
 
 @Component({
   selector: 'app-users-update-plot',
@@ -20,23 +18,19 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './users-update-plot.component.html',
   styleUrl: './users-update-plot.component.css'
 })
-export class UsersUpdatePlotComponent implements OnInit {
+export class UsersUpdatePlotComponent implements OnInit, OnDestroy {
 
   private readonly plotService = inject(PlotService);
   private readonly fileService = inject(FileService);
+  private readonly suscriptionService = inject(SuscriptionManagerService);
 
   types: PlotTypeModel[] = [];
   states: PlotStateModel[] = [];
   existingFiles: File[] = [];
   existingFilesDownload: FileDto[] = [];
   files: File[] = this.existingFiles;
-  
 
-  redirect(url: string) {
-    this.router.navigate([url]);
-  }
-
-  constructor(private router: Router, private route: ActivatedRoute){ }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   formReactivo = new FormGroup({
     plotNumber: new FormControl(0, [Validators.required, Validators.min(1)]),
@@ -50,46 +44,46 @@ export class UsersUpdatePlotComponent implements OnInit {
   ngOnInit(): void {
 
     var id = Number(this.route.snapshot.paramMap.get('id')) || 0;
-  
+
     this.formReactivo.get('plotNumber')?.disable();
     this.formReactivo.get('blockNumber')?.disable();
-  
+
     // Obtener el lote por su ID
-    this.plotService.getPlotById(id).subscribe({
+    const sus = this.plotService.getPlotById(id).subscribe({
       next: (response) => {
         console.log(response);
-  
+
         // Cargar los valores en el formulario
         this.formReactivo.get('plotNumber')?.setValue(response.plot_number);
         this.formReactivo.get('blockNumber')?.setValue(response.block_number);
         this.formReactivo.get('totalArea')?.setValue(response.total_area_in_m2);
         this.formReactivo.get('totalBuild')?.setValue(response.built_area_in_m2);
-  
+
         // Guardar el valor del nombre del estado y tipo para luego asignar el ID
         const plotStateName = response.plot_state;
         const plotTypeName = response.plot_type;
 
-        if(response.files.length > 0){
+        if (response.files.length > 0) {
           this.existingFilesDownload = response.files;
         }
 
-       if (response.files && response.files.length > 0) {
-         for (const fileDto of response.files) {
+        if (response.files && response.files.length > 0) {
+          for (const fileDto of response.files) {
 
-           this.fileService.getFile(fileDto.uuid).subscribe(({ blob, filename }) => {
-             // Crear un nuevo objeto File a partir del Blob
-             const newFile = new File([blob], filename, { type: blob.type });
-             this.existingFiles.push(newFile);
-             
-           }, error => {
-             console.error(`Error al descargar el archivo ${fileDto.uuid}, error`);
-           });
-           console.log("Files list after loading: ", this.existingFiles);
-         }
-       }
-  
+            this.fileService.getFile(fileDto.uuid).subscribe(({ blob, filename }) => {
+              // Crear un nuevo objeto File a partir del Blob
+              const newFile = new File([blob], filename, { type: blob.type });
+              this.existingFiles.push(newFile);
+
+            }, error => {
+              console.error(`Error al descargar el archivo ${fileDto.uuid}, error`);
+            });
+            console.log("Files list after loading: ", this.existingFiles);
+          }
+        }
+
         // Después de cargar los tipos y estados, encontrar el ID correcto
-        this.plotService.getAllTypes().subscribe({
+        const sus2 = this.plotService.getAllTypes().subscribe({
           next: (data) => {
             this.types = data;
             // Encontrar el ID del tipo por el nombre
@@ -100,43 +94,26 @@ export class UsersUpdatePlotComponent implements OnInit {
           },
           error: (error) => {
             console.error('Error al obtener los tipos de lote:', error);
-            alert("Error al obtener los tipos de lote!");
           }
         });
-  
-        this.plotService.getAllStates().subscribe({
-          next: (data) => {
-            this.states = data;
-            // Encontrar el ID del estado por el nombre
-            const plotState = this.states.find(s => s.name === plotStateName);
-            if (plotState) {
-              this.formReactivo.get('state')?.setValue(plotState.id.toString());
-            }
-          },
-          error: (error) => {
-            console.error('Error al obtener los estados de lote:', error);
-            alert("Error al obtener los estados de lote!");
-          }
-        });
+        //Agregar suscripción
+        this.suscriptionService.addSuscription(sus2);
       },
       error: (error) => {
         console.error('Error al obtener el lote:', error);
-        alert("Error al obtener el lote!");
       }
     });
-  }
-  
-  getFiles(files: File[]) {
-    this.files = files;
+    //Agregar suscripción
+    this.suscriptionService.addSuscription(sus);
   }
 
-  confirmExit() {
-    this.redirect('/home/plots/list');
+  //Desuscribirse de todas los observables
+  ngOnDestroy(): void {
+    this.suscriptionService.unsubscribeAll();
   }
 
-  
-
-  updatePlot(){
+  //Modificar lote
+  updatePlot() {
     var id = Number(this.route.snapshot.paramMap.get('id')) || 0;
     const plot: PutPlot = {
       total_area_in_m2: this.formReactivo.get('totalArea')?.value || 0,
@@ -147,11 +124,8 @@ export class UsersUpdatePlotComponent implements OnInit {
       files: this.files
     }
 
-    console.log(plot);
-    
-
-    this.plotService.putPlot(id, plot).subscribe({
-      next: (response) => {
+    const sus = this.plotService.putPlot(id, plot).subscribe({
+      next: () => {
         Swal.fire({
           icon: "success",
           title: "Se han guardado los cambios",
@@ -162,44 +136,30 @@ export class UsersUpdatePlotComponent implements OnInit {
           timer: undefined,
 
         });
-        
+
+        //Redirigir a la lista de lotes
         this.redirect('home/plots/list');
       },
       error: (error) => {
         console.log("Error al actualizar el lote" + error);
 
-          Swal.fire({
-            icon: "error",
-            title: "Ha ocurrido un error",
-            showConfirmButton: true,
-            confirmButtonText: "Aceptar",
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            timer: undefined,
-          });
+        Swal.fire({
+          icon: "error",
+          title: "Ha ocurrido un error",
+          showConfirmButton: true,
+          confirmButtonText: "Aceptar",
+          allowOutsideClick: false,
+          allowEscapeKey: false,
+          timer: undefined,
+        });
       }
     });
-  }
-  downloadFile(fileId: string) {
-    this.fileService.getFile(fileId).subscribe(({ blob, filename }) => {
-      // Crear una URL desde el Blob
-      const url = window.URL.createObjectURL(blob);
 
-      // Crear un enlace de descarga dinámico
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = filename;  // Nombre del archivo obtenido desde el encabezado
-      document.body.appendChild(a);
-      a.click();  // Simular el clic para descargar el archivo
-
-      // Limpiar el DOM y liberar el Blob después de la descarga
-      window.URL.revokeObjectURL(url);
-      a.remove();
-    }, error => {
-      console.error('Error al descargar el archivo', error);
-    });
+    //Agregar suscripción
+    this.suscriptionService.addSuscription(sus);
   }
 
+  //--------------------------------------------------Validaciones--------------------------------------------------
 
   //Retorna una clase para poner el input en verde o rojo dependiendo si esta validado
   onValidate(controlName: string) {
@@ -210,14 +170,14 @@ export class UsersUpdatePlotComponent implements OnInit {
     }
   }
 
-
+  //Muestra el mensaje de error personalizado
   showError(controlName: string): string {
     const control = this.formReactivo.get(controlName);
-    
+
     //Ver si el control existe y si tiene errores
     if (control && control.errors) {
       const [errorKey] = Object.keys(control.errors);
-  
+
       switch (errorKey) {
         case 'required':
           return 'Este campo no puede estar vacío.';
@@ -237,9 +197,16 @@ export class UsersUpdatePlotComponent implements OnInit {
           return 'Error no identificado en el campo.';
       }
     }
-    
+
     //Si no hay errores
     return '';
+  }
+
+  //--------------------------------------------------Archivos------------------------------------------------
+
+  //Obtener los archivos seleccionados
+  getFiles(files: File[]) {
+    this.files = files;
   }
 
   //Evento para actualizar el listado de files a los seleccionados actualmente
@@ -247,4 +214,36 @@ export class UsersUpdatePlotComponent implements OnInit {
     this.files = Array.from(FileList = event.target.files); //Convertir FileList a Array
   }
 
+  //Descargar archivo
+  downloadFile(fileId: string) {
+    this.fileService.getFile(fileId).subscribe(({ blob, filename }) => {
+      //Crear una URL desde el Blob
+      const url = window.URL.createObjectURL(blob);
+
+      //Crear un enlace de descarga dinámico
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = filename;  //Nombre del archivo obtenido desde el encabezado
+      document.body.appendChild(a);
+      a.click();  //Simular el clic para descargar el archivo
+
+      //Limpiar el DOM y liberar el Blob después de la descarga
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    }, error => {
+      console.error('Error al descargar el archivo', error);
+    });
+  }
+
+  //--------------------------------------------------Redirecciones------------------------------------------------
+
+  //Redireccionar
+  redirect(url: string) {
+    this.router.navigate([url]);
+  }
+
+  //Confirmar salida
+  confirmExit() {
+    this.redirect('/home/plots/list');
+  }
 }
