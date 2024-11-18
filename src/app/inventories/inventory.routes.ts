cac8eeef13e65d34d2712cad77331495510ad39a
112796-Agustin-Abraham-Loutaif <112796@tecnicatura.frc.utn.edu.ar>
2024-11-18 17:35:40 -0300

    Cambio ruteo tomi

--- src/app/inventories/inventory.routes.ts
+++ src/app/inventories/inventory.routes.ts
@@ -1,21 +1,23 @@
 import { Routes } from "@angular/router";
 import { InventoryEmployeeProviderHomeComponent } from "./inventory-employee-provider-home/inventory-employee-provider-home.component";
-import { IepDetailTableComponent } from "./iep-inventory/components/iep-detail-table/iep-detail-table.component";
 import { IepInventoryComponent } from "./iep-inventory/components/iep-inventory/iep-inventory.component";
 import { IepStockIncreaseComponent } from "./iep-inventory/components/iep-stock-increase/iep-stock-increase.component";
 import { IepTableComponent } from "./iep-inventory/components/iep-table/iep-table.component";
-import { IepWarehouseMovementSearchComponent } from "./iep-inventory/components/iep-warehouse-movement-search/iep-warehouse-movement-search.component";
 import { IepCategoriesListComponent } from "./iep-inventory/components/iep-categories-list/iep-categories-list.component";
 import { IepChartsInventoryComponent } from "./iep-inventory/components/iep-charts-inventory/iep-charts-inventory.component";
+import { IepProductComponent } from "./iep-inventory/components/iep-product/iep-product.component";
+import { IepSuppliersFormComponent } from "./iep-inventory/components/iep-suppliers-form/iep-suppliers-form.component";
 
 export const INVENTORY_ROUTES: Routes = [
 
  { path: '', component: InventoryEmployeeProviderHomeComponent }, //agregado para desarrollar
     
- { path: 'inventory', component: IepInventoryComponent },
-    { path: 'stock-increase', component: IepStockIncreaseComponent },
+    { path: 'inventory', component: IepInventoryComponent },
+    { path: 'new-product', component: IepProductComponent},
+    { path: 'product-update/:id', component: IepProductComponent },
     { path: 'stock-movements-history', component: IepTableComponent },
-    { path: 'warehouse-movements', component: IepWarehouseMovementSearchComponent },
-    { path: 'categories', component: IepCategoriesListComponent }, 
+    { path: 'stock-increase', component: IepStockIncreaseComponent },
+    { path: 'new-provider', component: IepSuppliersFormComponent },
+    { path: 'categories-list', component: IepCategoriesListComponent }, 
     { path: 'dashboard', component: IepChartsInventoryComponent }, 
 ];
