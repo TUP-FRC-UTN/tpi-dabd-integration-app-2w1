fec9db83feed24be53c719770ab66cab3defcdda
GabrielDM <113857@tecnicatura.frc.utn.edu.ar>
2024-11-30 20:10:48 -0300

    Kpis and charts changes

--- src/app/common/components/custom-kpi/custom-kpi.component.ts
+++ src/app/common/components/custom-kpi/custom-kpi.component.ts
@@ -8,16 +8,46 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './custom-kpi.component.html',
   styleUrls: ['./custom-kpi.component.css']
 })
-export class CustomKpiComponent{
+export class CustomKpiComponent {
 
   constructor() { }
 
-  @Input() amount : number | null | undefined = 0;
-  @Input() title : string ='';
-  @Input() subTitle: string='';
-  @Input() tooltip: string='';
+  @Input() value: any = 0;
+  @Input() title: string = '';
+  @Input() subTitle: string = '';
+  @Input() tooltip: string = '';
   @Input() customStyles: { [key: string]: string } = {};
-  @Input() icon: string='';
-  @Input() formatPipe: string='';
+  @Input() icon: string = '';
+  @Input() formatPipe: string = '';
 
+
+  getValue() {
+    let formattedValue: any;
+    switch (this.formatPipe) {
+      case 'currency': {
+        formattedValue = this.value.toLocaleString('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        break;
+      }
+
+      case 'percentage': {
+        formattedValue = (this.value / 100).toLocaleString('en-US', { style: 'percent', minimumFractionDigits: 0, maximumFractionDigits: 2 });
+        formattedValue += '%';
+        alert(formattedValue);
+        break;
+      }
+
+      case 'text': {
+        formattedValue = this.value.toString();
+        break;
+      }
+
+      default: {
+        formattedValue = this.value.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 2 });
+        break;
+      }
+
+    }
+
+    return formattedValue;
+  }
 }
