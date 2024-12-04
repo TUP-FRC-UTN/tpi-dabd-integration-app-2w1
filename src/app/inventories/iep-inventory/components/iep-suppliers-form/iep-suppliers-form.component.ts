430040efd57b71c7670aa87bad8b2db2da505dd4
EbeltramoUtn <405156@tecnicatura.frc.utn.edu.ar>
2024-12-04 18:43:10 -0300

    changed validator from phone number and cuit in providers

--- src/app/inventories/iep-inventory/components/iep-suppliers-form/iep-suppliers-form.component.ts
+++ src/app/inventories/iep-inventory/components/iep-suppliers-form/iep-suppliers-form.component.ts
@@ -26,7 +26,7 @@ export class IepSuppliersFormComponent {
       name: ['', Validators.required],
       cuit: ['', [
         Validators.required, this.validarCUIT()]],
-      phoneNumber: ['', [Validators.required, Validators.pattern('[1-9]{10}$')]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}$')]],
       createdUser:[this.userService.getUser().id],
       email: ['', [Validators.required, Validators.email, this.emailDomainValidator]],
       supplierType: ['OTHER', Validators.required],
@@ -198,7 +198,7 @@ export class IepSuppliersFormComponent {
         // Verifica que los primeros 2 dígitos sean un tipo válido (20, 23, 24, 27, 30, 33, 34)
         const tipo = parseInt(cuilLimpio.substring(0, 2), 10);
         console.log(tipo);
-        const tiposValidos = [30, 33, 34];
+        const tiposValidos = [20, 23, 24, 27, 30, 33, 34];
         if (!tiposValidos.includes(tipo)) {
           console.log("no es valido")
           return { cuilInvalido: true };
