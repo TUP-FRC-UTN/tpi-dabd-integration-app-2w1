68e9a733e047737b2740470c27e8a59abe7c84e7
GabrielDM <113857@tecnicatura.frc.utn.edu.ar>
2024-12-04 18:15:46 -0300

    fixes landing

--- src/app/common/components/users-side-button/users-side-button.component.ts
+++ src/app/common/components/users-side-button/users-side-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, SimpleChanges } from '@angular/core';
 import { SideButton } from '../../models/SideButton';
 import { CommonModule } from '@angular/common';
 import { RoutingService } from '../../services/routing.service';
@@ -11,6 +11,8 @@ import { RoutingService } from '../../services/routing.service';
   styleUrl: './users-side-button.component.css'
 })
 export class UsersSideButtonComponent {
+  //Maneja el ciclo de vida del componente
+  showComponent = true;
 
   //Expandir o cerrar
   @Input() expanded: boolean = false;
@@ -21,11 +23,31 @@ export class UsersSideButtonComponent {
   //Rol del usuario logeado
   @Input() userRole: string = "";
 
+  //Activa la emision de datos
   @Output() sendInfo = new EventEmitter<any>();
 
+
+  //Detecta cambios en el rol del usuario
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['userRole'] && this.info.childButtons) {
+        console.log(`Actualizando side button "${this.info.name}"`);
+        
+        this.resetComponent();
+    }
+  }
+
+  
+  //Envia la info al padre para la redireccion
   send(path: string, title: string) {
     this.sendInfo.emit({ path: path, title: title });
   }
-  
-  
+
+
+  //Reinicia el componente
+  resetComponent() {
+    this.showComponent = false;
+    setTimeout(() => this.showComponent = true, 0); //Pone un delay para que se reinicie
+  }
+
+
 }
