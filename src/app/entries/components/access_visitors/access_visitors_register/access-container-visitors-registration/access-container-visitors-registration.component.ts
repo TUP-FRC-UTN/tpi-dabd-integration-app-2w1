e832ea13147f16a13ee55a5e71fc1955ac8aef19
405516_Gonzalez_Federico <405516@tecnicatura.frc.utn.edu.ar>
2024-12-05 06:45:50 -0300

    .

--- src/app/entries/components/access_visitors/access_visitors_register/access-container-visitors-registration/access-container-visitors-registration.component.ts
+++ src/app/entries/components/access_visitors/access_visitors_register/access-container-visitors-registration/access-container-visitors-registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Subject, Subscription, combineLatest } from 'rxjs';
@@ -13,6 +13,7 @@ import { AccessVisitorsRegisterServiceService } from '../../../../services/acces
 import { AccessVisitorFormComponent } from "../access-visitor-form/access-visitor-form.component";
 import { AuthService } from '../../../../../users/users-servicies/auth.service';
 import { UserLoged } from '../../../../../users/users-models/users/UserLoged';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-access-container-visitors-registration',
   standalone: true,
@@ -27,6 +28,10 @@ import { UserLoged } from '../../../../../users/users-models/users/UserLoged';
 ],
 })
 export class AccessContainerVisitorsRegistrationComponent implements OnInit, OnDestroy {
+  private router=inject(Router);
+  goTo(path: string) {
+  this.router.navigate([path]);
+  }
   uid?: string;
   qrCodeId?: string;
   isQRCodeAvailable: boolean = false;
